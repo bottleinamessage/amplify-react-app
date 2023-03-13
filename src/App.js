6f2cd4247e006c9051e8712dd29f27ada44837cb
bottleinamessage <rollmop.flosses.0z@icloud.com>
2023-03-12 23:30:24 -0500

    ch2 labs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import { API } from 'aws-amplify';
 
-function App() {
+
+const App = () => {
+  // Create coins variable and set to empty array
+  const [coins, updateCoins] = useState([])
+
+  // Create additional state to hold user input for limit and start properties
+  const [input, updateInput] = useState({ limit: 5, start: 0 })
+
+  // Define function to all API
+  const fetchCoins = async () => {
+    const { limit, start } = input;
+    const data = await API.get('cryptoapi', `/coins?limit=${limit}&start=${start}`);
+    updateCoins(data.coins);
+  }
+
+  // Create a new function to allow users to update the input values
+  const updateInputValues = (type, value) => {
+    updateInput({ ...input, [type]: value });
+  }
+
+
+  // Call fetchCoins function when component loads
+  useEffect(() => {
+    fetchCoins()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        placeholder="start"
+        onChange={e => updateInputValues('start', e.target.value)}
+      />
+      <input
+        onChange={e => updateInputValues('limit', e.target.value)}
+        placeholder="limit"
+      />
+
+      <button onClick={fetchCoins}>Fetch Coins</button>
+      {
+        coins.map((coin, index) => (
+          <div key={index}>
+            <h2>{coin.name} - {coin.symbol}</h2>
+            <h5>${coin.price_usd}</h5>
+          </div>
+        ))
+
+      }
     </div>
   );
 }
 
-export default App;
+export default App
